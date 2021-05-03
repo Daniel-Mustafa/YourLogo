a6fb7bda49ea29a00a1cb426b3c4ff0b6bec8078
Daniel-Mustafa <83565485+Daniel-Mustafa@users.noreply.github.com>
2021-05-03 03:51:42 -0300

    Update Elements_Compras.js

--- cypress/support/elements/Elements_Compras.js
+++ cypress/support/elements/Elements_Compras.js
@@ -1,8 +1,22 @@
 class Elements_Compras {
     
 //Compra de uma Tshirt
-Women = () => { return '.tree > :nth-child(1)' }    
-Tshirt = () => { return '.block_content > .tree > :nth-child(1)' }
+Women = () => { return 'ul.sf-menu li' }
+Tshirt = () => { return ':nth-child(1) > .product-container > .right-block > .button-container > .ajax_add_to_cart_button > span' }
+Compra_Tshirt = () => { return '.button-container > .button-medium > span' }
+Ok_Tshirt = () => { return '.icon-ok' }
+Confirma_Compra = () => { return '.cart_navigation > .button > span' }
+Checkbox = () => { return '#cgv' }
+Cartao = () => { return '.bankwire' }
+Orders = () => { return '.button-exclusive' }
+
+//Confirmação de Dados Pessoais
+Label_Nome = () => { return '#address_delivery > .address_firstname' }
+Label_Company = () => { return '#address_delivery > .address_company' }
+Label_Phone = () => { return '#address_delivery > .address_phone_mobile' }
+Txt_Nome = () => { return 'Daniel Mustafa' }
+Txt_Company = () => { return 'Casa Magalhaes' }
+Txt_Phone = () => { return '+5585999060999'}
 }
 
 export default Elements_Compras;
